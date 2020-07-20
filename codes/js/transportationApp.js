87115cc1eaab046f182843c7f7fcc6c9c65f7844
ajwwlswotjd <ajwwlswotjd@naver.com>
2020-07-21 02:00:23 +0900

    일단 다 만들긴 했는뎅... 코드가 많이 더럽습니다..

--- codes/js/transportationApp.js
+++ codes/js/transportationApp.js
@@ -3,7 +3,13 @@ window.addEventListener("load",()=>{
 	fetch('data/transportation.json').then((e)=>{return e.json()}).then(transportations=>{
 		$.getJSON('data/reservation.json',(events)=>{
 			$.getJSON('data/transportation_reservation.json',(reservations)=>{
-				
+
+				$("#toast-close").on("click",e=> $("#toast").fadeOut());
+
+				$("#res_select").on("change",function(e){
+					$("#reserve_left").val(e.target.value);
+				});
+
 				$("#transportation_date").on("input",(e)=>{
 					let value = e.target.value;
 					$("#res_date").val(value);
@@ -31,8 +37,10 @@ window.addEventListener("load",()=>{
 						$(div).on("click",()=>{
 							$("#res_select").html("");
 							$("#res_transport").val(item.name);
+							$("#reserve_price").val(item.price);
 							let start = parseInt(item.cycle[0].split(":")[0])*60 + parseInt(item.cycle[0].split(":")[1]);
 							let end = parseInt(item.cycle[1].split(":")[0])*60 + parseInt(item.cycle[1].split(":")[1]);
+
 							for(let i = start; i <= end; i+=item.interval*1){
 								let string = (Math.floor(i/60)+"").padStart(2,"0")+":"+(i%60+"").padStart(2,"0");
 								let date = $("#transportation_date").val();
@@ -40,24 +48,47 @@ window.addEventListener("load",()=>{
 								let total = 0;
 								let ft = reservations.transportation_reservation.filter(x=>{
 									return x.date == date && string == x.time && x.transportation == item.id;
-								}).forEach(x=>{
-									total += x.member.old;
-									total += x.member.adult;
-									total += x.member.kids;
+								}).forEach(z=>{
+									total += z.member.old;
+									total += z.member.adult;
+									total += z.member.kids;
 								});
 								let left = item.limit - total;
+								if(left == 0) continue;
 								string += " 남은자리 : "+left;
 								let option = document.createElement("option");
 								option.innerHTML = string;
-								option.setAttribute("value",string);
+								option.setAttribute("value",left);
+								if(i == start){
+									option.setAttribute("selected","selected");
+									$("#reserve_left").val(left);
+								}
 								$("#res_select").append(option);
 							}
-							$("#reserve").dialog();
+							// $(".reserve_spinner").spinner("destroy");
+							$(".reserve_spinner").spinner({
+								min : 0,
+								step: 1,
+								start:0,
+								spin:function(e,ui){
+									$(e.target).val(ui.value);
+									let price = $("#reserve_price").val();
+									let adult = $('#adult').val()*price;
+									let kids = $('#kids').val()*price*0.6;
+									let old = $('#old').val()*price;
+									old*= price <= 20000 ? 0 : price <= 100000 && price > 20000 ? 0.5 : 0.8;
+									let total = adult+kids+old;
+									$("#res_total").val(total.toLocaleString()+"원");
+								}
+							});
+							$("#reserve").dialog({width:320});
 						});
 						$(".transportation-list").append(div);
 					});
 
 				});
+
+
 
 			});
 
@@ -95,6 +126,9 @@ window.addEventListener("load",()=>{
 });
 
 function reserve_submit(e){
-
+	let total = 0;
+	document.querySelectorAll(".reserve_spinner").forEach(x=> total += x.value*1);
+	$("#toast-body").html($("#reserve_left").val() < total ? "인원이 너무 많습니다." : "결제가 진행되었습니다.");
+	$('#toast').fadeIn();
 	return false;
 }
